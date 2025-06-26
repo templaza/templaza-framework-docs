cffd2fc17a52d1758ea7cbb958117a5f0dca6f33
Sonny Le <sonlv@templaza.com>
2025-06-26 10:15:15 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://templaza.github.io',
+  url: 'https://docs.templaza.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
